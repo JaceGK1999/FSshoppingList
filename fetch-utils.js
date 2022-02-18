09f8c43cca22a63e5523dab12876fd5469eba5cc
Jacegk1999 <Jacegk1999@gmail.com>
2022-02-17 16:59:26 -0800

    added all fetch utils. misspelled git so everything is in one commit

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,7 +1,47 @@
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://uhmsxsfarryniihsuyry.supabase.co';
+const SUPABASE_KEY = 'yJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVobXN4c2ZhcnJ5bmlpaHN1eXJ5Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY0NDg1ODk5NSwiZXhwIjoxOTYwNDM0OTk1fQ.6nj7bcX8HwQyFQNgiIoCcY6u37Gv4ctD0Ivcfo9zsKQ'';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export async function createTodo(todo) {
+    const response = await client
+        .from('shoppings')
+        .insert({
+            todo: todo,
+            complete: false,
+            user_id: client.auth.user().id,
+        })
+        .single();
+
+
+    return checkError(response);
+}
+
+export async function deleteAllTodos() {
+    await client.from('shoppings').delete().match({ user_id: client.auth.user().id });
+
+}
+
+export async function getTodos() {
+    const response = await client
+        .from('shoppings')
+        .select()
+        .order('complete')
+        .match({ user_id: client.auth.user().id });
+
+
+    return checkError(response);
+}
+
+export async function completeTodo(id) {
+    const response = await client.from('shoppings').update({ complete: true }).match({
+        user_id: client.auth.user().id,
+        id: id,
+    });
+
+
+    return checkError(response);
+}
 
 export function getUser() {
     return client.auth.session() && client.auth.session().user;
