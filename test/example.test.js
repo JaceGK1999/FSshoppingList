7e76d897ed00e7a3a3f8ca4e0a266dbf312b2797
Jacegk1999 <Jacegk1999@gmail.com>
2022-02-17 17:33:23 -0800

    addec check error ans started test

--- test/example.test.js
+++ test/example.test.js
@@ -1,14 +1,24 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
 
+import { renderShoppingList } from '../render-utils.js';
+
 const test = QUnit.test;
+
+test('render shopping list', (expect) => {
+    const expected = '';
+
+    const actual = renderShoppingList('hello');
+
+    expect.outerHtml(actual, expected);
+});
 
 test('time to test a function', (expect) => {
     //Arrange
     // Set up your arguments and expectations
     const expected = true;
-    
-    //Act 
+
+    //Act
     // Call the function you're testing and set the result to a const
     const actual = true;
 
